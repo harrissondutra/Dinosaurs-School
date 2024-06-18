4cc2283e00bf3d674b42f4acc72e1c7d8544cbee
harrissondutra <harrissondutra@gmail.com>
2024-06-17 22:38:20 -0300

    Implementação da tela de Login

--- app/src/main/java/com/estudo/dinosaursschool/activities/MainActivity.kt
+++ app/src/main/java/com/estudo/dinosaursschool/activities/MainActivity.kt
@@ -4,9 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.estudo.dinosaursschool.ui.components.Toolbar
 import com.estudo.dinosaursschool.ui.theme.DinosaursSchoolTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,15 +28,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DinosaursSchoolTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                ) { innerPadding ->
-
+                Surface {
+                    App()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App() {
+    Column {
+       Scaffold (
+           topBar = {
+               Toolbar()
+           }
+       ){ paddingValues ->
+           Box(modifier = Modifier.padding(paddingValues)){
+            LoginScreen()
+           }
+
+       }
+    }
+
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun AppPreview() {
+    App()
+
+}
 
 
